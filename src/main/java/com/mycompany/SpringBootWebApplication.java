4c73e9594895fac1491bf4ec399a43ad1346d3fd
prahlad2018 <singh.prahlad@gmail.com>
2018-03-20 17:39:46 -0400

    commit

--- src/main/java/com/mycompany/SpringBootWebApplication.java
+++ src/main/java/com/mycompany/SpringBootWebApplication.java
@@ -1,13 +1,30 @@
 package com.mycompany;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.mycompany")
+@EnableJpaRepositories("com.mycompany.*")
+@ComponentScan(basePackages = { "com.mycompany.*" })
+@EntityScan("com.mycompany.*") 
 public class SpringBootWebApplication extends SpringBootServletInitializer {
 
+	@Autowired
+	DataSource dataSource;	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SpringBootWebApplication.class);
